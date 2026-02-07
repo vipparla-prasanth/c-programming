05f6284dcc26bb688d761c9ad8116a5aed10e7a3
Prasanth Vipparla <prasanthv865@gmail.com>
2026-02-07 08:21:12 +0530

    Improve code documentation with comments
    Added comments to enhance code readability and understanding.

--- calculator-switch-case.c
+++ calculator-switch-case.c
@@ -1,21 +1,23 @@
-#include<stdio.h>
+#include<stdio.h>   //including a header file for standard input and output
 int main()
 {
     int choice;
     float a,b;
     char loop;
-    do {
+    do {              //exit controlled loop
 
-    printf("\n1.add.\n2.sub.\n3.mul.\n4.div.\n");
-    printf("\nenter choice : ");
-    scanf("%d", &choice);
-    printf("enter two numbers : ");
-    scanf("%f%f",&a,&b);
+    printf("\n1.add.\n2.sub.\n3.mul.\n4.div.\n");    
+    printf("\nenter choice : ");          //asking the user to enter choice
+    scanf("%d", &choice);                 //used for reading the input choice
+    printf("enter two numbers : ");        //asking the user to enter two numbers
+    scanf("%f%f",&a,&b);                    //reading two floating point values
 
-    switch (choice) {
-        case 1 :
-            printf("the addition of %.2f and %.2f is %.2f\n",a,b,a+b);
-            break;
+    switch (choice) {                       //switch is to execute one block of code out of many based on the value of a single expression
+        case 1 :                            //case 1 : means when user entered number 1 these block of code will be executed
+            
+            printf("the addition of %.2f and %.2f is %.2f\n",a,b,a+b);         //performing and printing the added values of given two numbers 
+            
+            break;                          //break is used to terminate the switch immediatly                             
         case 2 :
             printf("the subraction of %.2f and %.2f is %.2f\n",a,b,a-b);
             break;
@@ -30,13 +32,14 @@ int main()
                 printf("the division of %.2f and %.2f is %.2f\n",a,b,a/b);
             }
             break;
-        default : 
+        default :                 //default statement is used when the input is outof range 
             printf("enter a valid choice!!, and try again\n");
             break;
     }
-    printf("do you want to try any other numbers : (Y/N)");
+    printf("do you want to try any other numbers : (Y/N)");  //asking the user to continue with other numbers or not 
     scanf(" %c",&loop);
-} while (loop=='Y' || loop=='y');
+} while (loop=='Y' || loop=='y');        //if user enter 'Y' OR 'y' the do while loop will continue 
     
-    return 0;
+    return 0;                //as we have used int in the main function we should return a value at the end of the program so we returned 0
+
 }
